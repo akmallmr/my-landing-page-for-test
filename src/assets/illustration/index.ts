4ba0a4daffb22651ac56b669733df40bbc33fd83
Akmal <am001512@abishar.com>
2024-12-28 17:10:10 +0700

    FEAT: add assets svg

---
+++ src/assets/illustration/index.ts
@@ -0,0 +1,8 @@
+import ILTeamUp from "./il-team-up.svg";
+import ILVegan from "./il-vegan.svg";
+import ILBorcelle from "./il-blue-borcelle.svg";
+import ILForkSpoon from "./il-fork-spoon.svg";
+import ILOrganLeaf from "./il-organ-leaf.svg";
+import ILOrganic from "./il-organic.svg";
+
+export { ILTeamUp, ILVegan, ILBorcelle, ILForkSpoon, ILOrganLeaf, ILOrganic };

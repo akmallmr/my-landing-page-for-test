f2a713bbffdd615984d47eed32f02723eb05a419
Akmal <am001512@abishar.com>
2024-12-29 10:30:32 +0700

    FIX: image render config domain

--- next.config.ts
+++ next.config.ts
@@ -1,5 +1,24 @@
 // next.config.js
 const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "static5.depositphotos.com",
+        pathname: "/**",
+      },
+      {
+        protocol: "https",
+        hostname: "static7.depositphotos.com",
+        pathname: "/**",
+      },
+      {
+        protocol: "https",
+        hostname: "st5.depositphotos.com",
+        pathname: "/**",
+      },
+    ],
+  },
   experimental: {
     turbo: {
       rules: {
@@ -11,14 +30,13 @@ const nextConfig = {
     },
   },
   webpack(config: any, { isServer }: any) {
-    // Adding the rule for handling SVG files for normal Webpack (fallback)
     config.module.rules.push({
       test: /\.svg$/,
       use: [
         {
           loader: "@svgr/webpack",
           options: {
-            icon: true, // Optional: Adds width/height automatically for icons
+            icon: true,
           },
         },
       ],

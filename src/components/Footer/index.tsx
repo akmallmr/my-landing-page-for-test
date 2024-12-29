53717021dd139d12a56ceb72ce1358f63a44889b
Akmal <am001512@abishar.com>
2024-12-29 10:31:13 +0700

    FEAT: add Footer

---
+++ src/components/Footer/index.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <div className="p-4 w-full flex flex-row gap-10 bg-gradient-to-r via-rose-400 from-pink-500 to-red-500 shadow-lg overflow-hidden justify-center items-center text-white text-center text-lg">
+      © For e-dot testing
+    </div>
+  );
+};
+
+export default Footer;
--- src/components/index.ts
+++ src/components/index.ts
@@ -1,4 +1,5 @@
 import Header from "./Header";
 import Gap from "./Gap";
+import Footer from "./Footer";
 
-export { Header, Gap };
+export { Header, Gap, Footer };

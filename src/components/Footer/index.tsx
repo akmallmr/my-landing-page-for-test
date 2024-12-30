cd166a81585465219fc2cc77488a872a1d86cc2d
akmallmr <akmallmr@gmail.com>
2024-12-30 22:00:23 +0700

    CHORE: linting " with '

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -1,8 +1,8 @@
-import React from "react";
+import React from 'react';
 
 const Footer = () => {
   return (
-    <div className="p-4 w-full flex flex-row gap-10 bg-gradient-to-r via-rose-400 from-pink-500 to-red-500 shadow-lg overflow-hidden justify-center items-center text-white text-center text-lg">
+    <div className='p-4 w-full flex flex-row gap-10 bg-gradient-to-r via-rose-400 from-pink-500 to-red-500 shadow-lg overflow-hidden justify-center items-center text-white text-center text-lg'>
       © For e-dot testing
     </div>
   );

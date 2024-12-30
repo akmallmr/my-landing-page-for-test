30f534cd72d7ad2a5faa46ac8ba46c934b237788
akmallmr <akmallmr@gmail.com>
2024-12-30 20:43:30 +0700

    CHORE: linting " and '

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,20 +1,20 @@
-import type { Metadata } from "next";
-import { Poppins } from "next/font/google";
-import { Footer, Header } from "@/components";
-import { MenuHeaderProvider } from "@/context/MenuHeaderContext";
-import { ProductProvider } from "@/context/ProductContext";
-import "./globals.css";
+import type { Metadata } from 'next';
+import { Poppins } from 'next/font/google';
+import { Footer, Header } from '@/components';
+import { MenuHeaderProvider } from '@/context/MenuHeaderContext';
+import { ProductProvider } from '@/context/ProductContext';
+import './globals.css';
 
 const poppins = Poppins({
-  variable: "--font-poppins",
-  subsets: ["latin"],
-  weight: ["400", "500", "700"], // Specify weights you use (e.g., regular, medium, bold)
-  display: "swap", // Improves font-loading behavior
+  variable: '--font-poppins',
+  subsets: ['latin'],
+  weight: ['400', '500', '600', '700'], // Specify weights you use (e.g., regular, medium, bold)
+  display: 'swap', // Improves font-loading behavior
 });
 
 export const metadata: Metadata = {
-  title: "CartX - Landing Page",
-  description: "Landing page CartX for testing edot purposed",
+  title: 'CartX - Landing Page',
+  description: 'Landing page CartX for testing edot purposed',
 };
 
 export default function RootLayout({
@@ -23,7 +23,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang='en'>
       <body className={`${poppins.variable} antialiased scrollbar-hide`}>
         <ProductProvider>
           <MenuHeaderProvider>

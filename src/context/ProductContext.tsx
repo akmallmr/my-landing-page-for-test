7f865542c742884eec231323935fc1ecd6b8e54b
akmallmr <akmallmr@gmail.com>
2024-12-30 21:04:19 +0700

    CHORE: adjustment code

--- src/context/ProductContext.tsx
+++ src/context/ProductContext.tsx
@@ -1,47 +1,30 @@
-"use client";
+'use client';
 
-import { getImageItems, getProductItems } from "@/api/product";
-import {
-  createContext,
-  ReactNode,
-  useContext,
-  useEffect,
-  useState,
-} from "react";
-import {
-  ImagesProps,
-  ProductProps,
-  ProductsContextProps,
-  ProductsProps,
-} from "./types";
+import { getImageItems, getProductItems } from '@/api/product';
+import { createContext, ReactNode, useContext, useEffect, useState } from 'react';
+import { ImagesProps, ProductProps, ProductsContextProps, ProductsProps } from './types';
 
-const ProductContext = createContext<ProductsContextProps | undefined>(
-  undefined
-);
+const ProductContext = createContext<ProductsContextProps | undefined>(undefined);
 
 const ProductProvider = ({ children }: { children: ReactNode }) => {
-  const [product, setProduct] = useState<ProductsProps[] | undefined>(
-    undefined
-  );
+  const [product, setProduct] = useState<ProductsProps[] | undefined>(undefined);
 
   const handleGetProduct = async () => {
     try {
       const responseProduct = await getProductItems();
       const responseImage = await getImageItems();
 
-      if (responseImage || responseImage) {
-        const combinedResponse = responseProduct?.data?.map(
-          (item: ProductProps) => {
-            const image_url = responseImage?.data?.find((img: ImagesProps) =>
-              img?.id?.includes(item?.id)
-            );
+      if (responseProduct || responseImage) {
+        const combinedResponse = responseProduct?.data?.map((item: ProductProps) => {
+          const image_url = responseImage?.data?.find((img: ImagesProps) =>
+            img?.id?.includes(item?.id)
+          );
 
-            return {
-              ...item,
-              image: image_url ? image_url.image : null,
-            };
-          }
-        );
+          return {
+            ...item,
+            image: image_url ? image_url.image : null,
+          };
+        });
 
         setProduct(combinedResponse);
       }
@@ -54,17 +37,13 @@ const ProductProvider = ({ children }: { children: ReactNode }) => {
     handleGetProduct();
   }, []);
 
-  return (
-    <ProductContext.Provider value={{ product }}>
-      {children}
-    </ProductContext.Provider>
-  );
+  return <ProductContext.Provider value={{ product }}>{children}</ProductContext.Provider>;
 };
 
 const useProductContext = () => {
   const context = useContext(ProductContext);
   if (!context) {
-    throw new Error("useProductContext must be used within a ProductProvider");
+    throw new Error('useProductContext must be used within a ProductProvider');
   }
   return context;
 };
